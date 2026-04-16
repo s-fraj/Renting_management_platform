f9c74187195d758f21d71e11452702a85283eee8
s-fraj <frajselmi2001sm@gmail.com>
2026-04-16 01:25:45 +0100

    Add files via upload

---
+++ pages/Home.jsx
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar.jsx";
+import MainSection from "../components/MainSection.jsx";
+import Footer from "../components/Footer.jsx";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <MainSection />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
